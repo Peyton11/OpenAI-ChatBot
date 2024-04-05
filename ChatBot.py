d114283dfa85a0bc1b77f8548e27c743ea1f390d
Peyton11 <peyton.gardner03@gmail.com>
2024-04-05 15:44:28 -0400

    Add comments to ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,9 +1,11 @@
 # ChatBot.py
-#
+# Implemenation for ChatBot class
 
 from openai import OpenAI
 
 class ChatBot:
+
+    # Initialize object with parameters. Prompt will be initialized from user input.
     def __init__(self, client, model, temperature, max_tokens, system_message):
         self.client = client
         self.model = model
@@ -17,9 +19,11 @@ class ChatBot:
             {"role": "system", "content": self.prompt}
         ]
 
+    # Sets the prompt
     def set_prompt(self, prompt):
         self.prompt = prompt
 
+    # Returns the AI's response after the prompt
     def get_summary(self):
         completion = self.client.chat.completions.create(
             model = self.model,
